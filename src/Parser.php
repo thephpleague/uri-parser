debe167a78ee4cbe9444ebf808c9d023cb8ada5e
Aaron Piotrowski <aaron@trowski.com>
2018-03-09 11:29:55 -0600

    Restore constants for BC

--- src/Parser.php
+++ src/Parser.php
@@ -26,12 +26,22 @@ namespace League\Uri;
  */
 class Parser
 {
+    const INVALID_URI_CHARS = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x7F";
+
+    const SCHEME_VALID_STARTING_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const SCHEME_VALID_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+.-';
+
+    const LABEL_VALID_STARTING_CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
     const LOCAL_LINK_PREFIX = '1111111010';
 
     const URI_COMPONENTS = [
         'scheme' => null, 'user' => null, 'pass' => null, 'host' => null,
         'port' => null, 'path' => '', 'query' => null, 'fragment' => null,
     ];
+
+    const SUB_DELIMITERS = '!$&\'()*+,;=';
 
     /**
      * Returns whether a scheme is valid.
